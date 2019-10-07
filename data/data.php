afcda7424c3fe2da5f2229065f3636ab10dc186a
cafemato <ccshao@gmail.com>
2019-10-07 23:32:27 +0800

    Add Chart.JS Graph to show keyword  job counts in industry

---
+++ data/data.php
@@ -0,0 +1,39 @@
+<?php
+//setting header to json
+header('Content-Type: application/json; charset=utf-8');
+
+//database
+define('DB_HOST', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'job_bank');
+
+//get connection
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+  die("Connection failed: " . $mysqli->error);
+}
+
+//query to get data from the table
+$query = sprintf("SELECT industry, count(*) AS counts FROM bigdata GROUP BY industry");
+
+//execute query
+$result = $mysqli->query($query);
+
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+  $data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+//now print the data
+print json_encode($data);
+
+?>

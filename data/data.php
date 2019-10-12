8e73224ad7048afc8010f081786a4eeedfcf7482
cafemato <ccshao@gmail.com>
2019-10-12 16:50:28 +0800

    Update data.php

--- data/data.php
+++ data/data.php
@@ -18,11 +18,27 @@ if(!$mysqli){
 }
 
 //query to get data from the table
+//查訊各產業的職缺數量
 $query = sprintf("SELECT industry, count(*) AS counts FROM bigdata GROUP BY industry ");
-$query2 = sprintf("SELECT * FROM bigdata");
+//查訊關鍵字各產業的最低起薪跟最高起薪
+$query2 = sprintf("SELECT company,salary_low,requirement from bigdata");
+//查訊關鍵字各產業的最低起薪跟最高起薪
+$query3 = sprintf("SELECT AVG(CASE WHEN industry = '資訊科技業' THEN salary_low END) AS  tech,
+                 AVG(CASE WHEN industry = '電子電信業' THEN salary_low END) AS  elec,
+                 AVG(CASE WHEN industry = '金融業' THEN salary_low END) AS  fin,
+                 AVG(CASE WHEN industry = '傳產製造業' THEN salary_low END) AS  manu,
+                 AVG(CASE WHEN industry = '文化媒體業' THEN salary_low END) AS  media,
+                 AVG(CASE WHEN industry = '教育研究醫療生技業' THEN salary_low END) AS  edu,
+                 AVG(CASE WHEN industry = '一般商業' THEN salary_low END) AS  biz,
+                 AVG(CASE WHEN industry = '服務業' THEN salary_low END) AS  service,     
+                 AVG(CASE WHEN industry = '其他產業' THEN salary_low END) AS  others
+				 FROM bigdata
+				 WHERE salary_low >1500 and salary_low < 150000 ");
+
 //execute query
 $result = $mysqli->query($query);
 $result2 = $mysqli->query($query2);
+$result3 = $mysqli->query($query3);
 
 //loop through the returned data
 $data = array();
@@ -34,8 +50,16 @@ $data2 = array();
 foreach ($result2 as $row) {
   $data2[] = $row;
 }
+
+$data3 = array();
+foreach ($result3 as $row) {
+  $data3[] = $row;
+}
 $posts = array();
-$posts[] = array('data1'=> $data, 'data2'=> $data2);
+$posts[] = array('data1'=> $data, 'data2'=> $data2, 'data3'=> $data3);
+
+
+// var myJSON = JSON.stringify(myObj);
 
 //free memory associated with result
 $result->close();
